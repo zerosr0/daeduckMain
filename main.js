2efd39721e007cc0429806eb5ea36336bca6e14f
zerosr0 <sinter3@naver.com>
2023-07-18 13:30:32 +0900

    대덕전자 1차 완성

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //개발팀별 project 현황
+
 Highcharts.chart('container_team', {
   chart: {
     type: 'column',
@@ -16,7 +17,9 @@ Highcharts.chart('container_team', {
       enabled: false
     },
     stackLabels: {
-      enabled: true
+      enabled: true,
+      pointFormat: '{point.stackTotal}건',
+      //누적현황 숫자 뒤에 '건' 추가(작동 안됨)
     }
   },
   legend: {
@@ -46,7 +49,6 @@ Highcharts.chart('container_team', {
   series: [{
     name: 'T1',
     data: [3, 5, 2],
-    size: 15,
     color: '#87DEA4',
 
   }, {
@@ -63,6 +65,7 @@ Highcharts.chart('container_team', {
     enabled: false
   }
 });
+
 
 //APQP 단계별
 Highcharts.chart('container_step', {
@@ -145,3 +148,77 @@ Highcharts.chart('container_step', {
 });
 
 
+//js 적용한 속성 미디어쿼리 적용
+if (matchMedia("screen and (max-width: 767px)").matches) {
+  //화면 크기가 767px이하일때
+  console.log("mobile");
+} else if (matchMedia("screen and (max-width: 1023px)").matches) {
+  //화면 크기가 1023px이하일때
+  console.log("tablet");
+} else if (matchMedia("screen and (min-width: 1450px)").matches) {
+  Highcharts.chart('container_team', {
+    chart: {
+      type: 'column',
+    },
+    title: {
+      text: '개발팀별 project 현황',
+      align: 'center'
+    },
+    xAxis: {
+      categories: ['정상', '지연예상', '지연']
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        enabled: false
+      },
+      stackLabels: {
+        enabled: true,
+        pointFormat: '{point.stackTotal}건',
+        //누적현황 숫자 뒤에 '건' 추가(작동 안됨)
+      }
+    },
+    legend: {
+      align: 'left',
+      x: 100,
+      verticalAlign: 'top',
+      y: 20,
+      floating: true,
+      backgroundColor:
+        Highcharts.defaultOptions.legend.backgroundColor || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+    },
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true
+        }
+      }
+    },
+    series: [{
+      name: 'T1',
+      data: [3, 5, 2],
+      color: '#87DEA4',
+
+    }, {
+      name: 'T2',
+      data: [12, 8, 8],
+      color: '#4472C4',
+    }, {
+      name: 'T3',
+      data: [2, 2, 5],
+      color: '#FDE367',
+    }],
+    //워터마크 제거
+    credits: {
+      enabled: false
+    }
+  });
+}

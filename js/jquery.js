dbf5f2a892562aa0cac166b94f76421bb04cc060
zero0 <sinter3@naver.com>
2023-09-11 18:04:07 +0900

    leftmenu jquery 1차 완성, 대메뉴 클릭시 leftmenu 변형

--- js/jquery.js
+++ js/jquery.js
@@ -6,23 +6,26 @@ $(function () {
 
   //첫번째 메뉴를 클릭하면 open 스타일 적용되게, open 스타일이 적용되어 있다면 그 하위메뉴 펼쳐지게
   $('li.step01 a').on("click", function () {
-    $(this).siblings('ul.step02').next().stop().slideDown(150);
-    $(this).toggleClass('open');
-    $(this).siblings('ul.step02').slideUp();
-    $(!this).removeClass('open'); //TODO
+    $('li.step02 a').removeClass('open2');
+    $(this).next().stop().slideToggle(150);
+    $(this).parent('li.step01').siblings('li.step01').children('ul.step02').slideUp(150);
+    $(this).addClass('open');
+    $(this).parent('li.step01').siblings('li.step01').children('a').removeClass('open');
   })
 
   //두번째 메뉴를 클릭하면 open2 스타일 적용되고 그 하위메뉴 펼쳐지게
   $('li.step02 a').on("click", function () {
+    $('li.step03 a').removeClass('open3');
     $('li.step02 a').eq(0).removeClass('open2');
-    $(this).toggleClass('open2');
-    $(this).siblings('ul.step03').slideToggle(150);
-    // $(!this).children('ul.step03').hide();
+    $(this).parent('li.step02').siblings('li.step02').children('ul.step03').slideUp(150);
+    $(this).addClass('open2');
+    $(this).parent('li.step02').siblings('li.step02').children('a').removeClass('open2');
   })
 
+  //세번째 메뉴를 클릭하면 open3 스타일 적용
   $('li.step03 a').on("click", function () {
     $(this).addClass('open3');
-    $(!this).removeClass('open3');
+    $(this).parent('li.step03').siblings('li.step03').children('a').removeClass('open3');
   })
 
   let sec_height = $('.tableFrame .tableSection').height;
@@ -35,35 +38,43 @@ $(function () {
   $('.main-gnb ul li:nth-of-type(1) > a').on("click", function () {
     $('.step01.myjob').children('ul.step02').slideToggle(150);
     $('.myjob > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
   $('.main-gnb ul li:nth-of-type(2)').on("click", function () {
     $('.step01.dev-request').children('ul.step02').slideToggle(150);
     $('.side-menu .dev-request > a').toggleClass('open');
+    $('.container').toggleClass('active');
     //$('.side-menu').not($('.product-info > a')).removeClass('open');
   })
   $('.main-gnb ul li:nth-of-type(3)').on("click", function () {
     $('.step01.dev-quality').children('ul.step02').slideToggle(150);
     $('.side-menu .dev-quality > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
   $('.main-gnb ul li:nth-of-type(4)').on("click", function () {
     $('.step01.open-issue').children('ul.step02').slideToggle(150);
     $('.side-menu .open-issue > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
   $('.main-gnb ul li:nth-of-type(5)').on("click", function () {
     $('.step01.past-quality').children('ul.step02').slideToggle(150);
     $('.side-menu .past-quality > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
   $('.main-gnb ul li:nth-of-type(6)').on("click", function () {
     $('.step01.change-manage').children('ul.step02').slideToggle(150);
     $('.side-menu .change-manage > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
   $('.main-gnb ul li:nth-of-type(7)').on("click", function () {
     $('.step01.review-manage').children('ul.step02').slideToggle(150);
     $('.side-menu .review-manage > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
   $('.main-gnb ul li:nth-of-type(8)').on("click", function () {
     $('.step01.dev-standard-manage').children('ul.step02').slideToggle(150);
     $('.side-menu .dev-standard-manage > a').toggleClass('open');
+    $('.container').toggleClass('active');
   })
 })
 

7b3c453bb0e3ef3cb90f3ba038a444819c8c37e8
zero0 <sinter3@naver.com>
2023-09-12 18:02:07 +0900

    leftmenu 버튼 추가, 제이쿼리 코드 정리

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,14 @@
 //leftmenu accordion
 $(function () {
   //화면 로드 시 MYJOB 부분은 펼쳐있고, MYJOB현황은 open2 스타일적용 
-  $('ul.step02').eq(0).show();
+  // $('ul.step02').eq(0).show();
   $('li.step02 a').eq(0).addClass('open2');
 
   //첫번째 메뉴를 클릭하면 open 스타일 적용되게, open 스타일이 적용되어 있다면 그 하위메뉴 펼쳐지게
   $('li.step01 a').on("click", function () {
     $('li.step02 a').removeClass('open2');
     $(this).next().stop().slideToggle(150);
-    $(this).parent('li.step01').siblings('li.step01').children('ul.step02').slideUp(150);
+    $(this).parent('li.step01').siblings('li.step01').children('ul.step02').slideUp();
     $(this).addClass('open');
     $(this).parent('li.step01').siblings('li.step01').children('a').removeClass('open');
   })
@@ -35,46 +35,17 @@ $(function () {
   $('.tableFrame .tableSection .th').css('height', sec_height + 'px');
 
   //대메뉴 클릭시 왼쪽 사이드 메뉴 하위메뉴 toggle
-  $('.main-gnb ul li:nth-of-type(1) > a').on("click", function () {
-    $('.step01.myjob').children('ul.step02').slideToggle(150);
-    $('.myjob > a').toggleClass('open');
+  $('.main-gnb > ul > li').on("click", function () {
+    let index = $(this).index();
+    $('.side-menu > li > ul').eq(index).slideDown(150);
+    $('.side-menu > li > ul').not($('.side-menu > li > ul').eq(index)).slideUp(150);
     $('.container').toggleClass('active');
+    $('.slide-btn').addClass('active');
   })
-  $('.main-gnb ul li:nth-of-type(2)').on("click", function () {
-    $('.step01.dev-request').children('ul.step02').slideToggle(150);
-    $('.side-menu .dev-request > a').toggleClass('open');
-    $('.container').toggleClass('active');
-    //$('.side-menu').not($('.product-info > a')).removeClass('open');
-  })
-  $('.main-gnb ul li:nth-of-type(3)').on("click", function () {
-    $('.step01.dev-quality').children('ul.step02').slideToggle(150);
-    $('.side-menu .dev-quality > a').toggleClass('open');
-    $('.container').toggleClass('active');
-  })
-  $('.main-gnb ul li:nth-of-type(4)').on("click", function () {
-    $('.step01.open-issue').children('ul.step02').slideToggle(150);
-    $('.side-menu .open-issue > a').toggleClass('open');
-    $('.container').toggleClass('active');
-  })
-  $('.main-gnb ul li:nth-of-type(5)').on("click", function () {
-    $('.step01.past-quality').children('ul.step02').slideToggle(150);
-    $('.side-menu .past-quality > a').toggleClass('open');
-    $('.container').toggleClass('active');
-  })
-  $('.main-gnb ul li:nth-of-type(6)').on("click", function () {
-    $('.step01.change-manage').children('ul.step02').slideToggle(150);
-    $('.side-menu .change-manage > a').toggleClass('open');
-    $('.container').toggleClass('active');
-  })
-  $('.main-gnb ul li:nth-of-type(7)').on("click", function () {
-    $('.step01.review-manage').children('ul.step02').slideToggle(150);
-    $('.side-menu .review-manage > a').toggleClass('open');
-    $('.container').toggleClass('active');
-  })
-  $('.main-gnb ul li:nth-of-type(8)').on("click", function () {
-    $('.step01.dev-standard-manage').children('ul.step02').slideToggle(150);
-    $('.side-menu .dev-standard-manage > a').toggleClass('open');
-    $('.container').toggleClass('active');
+
+  //slide-btn 누르면 leftmenu 숨김처리
+  $('.slide-btn').on("click", function () {
+    $('.container').removeClass('active');
   })
 })
 
